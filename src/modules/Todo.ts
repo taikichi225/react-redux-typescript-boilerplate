7b550ba259afa7613fcb49275fba3d8029ba1f26
taiki <t.kohno225@gmail.com>
2018-07-07 14:32:26 +0900

    コメント追加

--- src/modules/Todo.ts
+++ src/modules/Todo.ts
@@ -1,13 +1,16 @@
+/** Action */
 export interface Todo {
   id: number
   completed: boolean
   text: string
 }
 
+/** Action */
 export type Todos = Todo[]
 
 let id: number = 0
 
+/** Action Creators */
 export const generateTodo = (text: string, id: number): Todo => {
   return {
     id,
@@ -16,6 +19,7 @@ export const generateTodo = (text: string, id: number): Todo => {
   }
 }
 
+/** Action Creators */
 export const generateTodos = (text: string[]): Todo[] => {
   return text.map(t => {
     let todo = generateTodo(t, id)
@@ -26,24 +30,29 @@ export const generateTodos = (text: string[]): Todo[] => {
 
 let nextTodoId = 0
 
+/** Action Type */
 export enum TodoActionType {
   ADD_TODO = 'ADD_TODO',
   TOGGLE_TODO = 'TOGGLE_TODO'
 }
 
+/** Action */
 export interface AddTodoAction {
   type: TodoActionType.ADD_TODO
   id: number
   text: string
 }
 
+/** Action */
 export interface ToggleTodoAction {
   type: TodoActionType.TOGGLE_TODO
   id: number
 }
 
+/** Action */
 export type TodoAction = AddTodoAction | ToggleTodoAction
 
+/** Action Creators */
 export function addTodo(text: string): AddTodoAction {
   return {
     type: TodoActionType.ADD_TODO,
@@ -52,6 +61,7 @@ export function addTodo(text: string): AddTodoAction {
   }
 }
 
+/** Action Creators */
 export function toggleTodo(id: number): ToggleTodoAction {
   return {
     type: TodoActionType.TOGGLE_TODO,
@@ -59,6 +69,7 @@ export function toggleTodo(id: number): ToggleTodoAction {
   }
 }
 
+/** Reducers */
 function todos(state: Todos, action: TodoAction): Todos {
   switch (action.type) {
     case TodoActionType.ADD_TODO:
